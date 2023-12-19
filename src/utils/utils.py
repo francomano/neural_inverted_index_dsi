bc74aef1c38633d7339bc7f8e7900fe8b012378d
Luigi <gallo.1895146@studenti.uniroma1.it>
2023-12-19 23:54:39 +0100

    added utils

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -2,6 +2,7 @@ import difflib
 import random
 import numpy as np
 from sklearn.metrics import precision_score
+import torch
 
 def evaluate_precision_at_k(corpus, tfidf_matrix, num_queries_to_sample, k):
     # Extract all queries from the corpus
@@ -45,3 +46,46 @@ def document_embedding(doc_tokens, word2vec_model):
     if not word_embeddings:
         return np.zeros(word2vec_model.vector_size)
     return np.mean(word_embeddings, axis=0)
+
+
+
+def evaluate_query(siamese_model, query_and_document_embeddings, query_index, k=100, threshold=0.6):
+    # Define the device
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # Initialize sets for retrieved and relevant documents
+    retrieved_docs = set()
+    relevant_docs = set()
+
+    # Set the model to evaluation mode
+    siamese_model.eval()
+
+    # Retrieve the query embedding
+    my_query = query_and_document_embeddings[query_index][0]
+
+    # Iterate over each query, document, relevance, and document id in the embeddings
+    for query, doc, relevance, id in query_and_document_embeddings:
+        # Predict the relevance
+        pred = siamese_model(torch.from_numpy(my_query).unsqueeze(-1).permute(1,0).to(device),
+                             torch.from_numpy(doc).unsqueeze(-1).permute(1,0).to(device))
+        if pred > threshold:
+            tuple_to_add = (int(id.item()), float(pred))
+            retrieved_docs.add(tuple_to_add)
+            if torch.equal(torch.from_numpy(query), torch.from_numpy(my_query)) and relevance.item() == 1: 
+                relevant_docs.add(tuple_to_add)
+
+    # Sort and select top k retrieved documents
+    retrieved_docs_sorted = sorted(retrieved_docs, key=lambda x: x[1], reverse=True)
+    top_k = retrieved_docs_sorted[:k]
+
+    # Calculate the number of relevant documents in top k
+    rel = sum(1 for docid in top_k if docid in relevant_docs)
+
+    # Return the precision metrics
+    return {
+        "query_index": query_index,
+        "retrieved_documents": k,
+        "precision_at_k": rel / k,
+        "relevant_docids": relevant_docs,
+        "top_k_retrieved_docids": top_k
+    }
