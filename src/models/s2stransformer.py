04c1489259ba4b153b9618acc1895fcffa086d07
francomano <playerhorus@gmail.com>
2024-01-02 11:13:33 +0100

    added norm layer, dropout, dynamic masking and orthogonal init of the layers

--- src/models/s2stransformer.py
+++ src/models/s2stransformer.py
@@ -1,10 +1,9 @@
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-import torch.nn as nn
-import pytorch_lightning as pl
+from torch.optim.lr_scheduler import LambdaLR
 import math
-
 
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model):
@@ -20,13 +19,13 @@ class PositionalEncoding(nn.Module):
         pe[:, 0, 1::2] = torch.cos(position * div_term[:feature_dim//2])
         pe = pe.unsqueeze(0)
 
-        # Add positional encoding
-        x_with_pe = (x + pe).squeeze(0)
+        # Add positional encoding and apply layer normalization
+        x_with_pe = F.layer_norm(x + pe, x.size()[1:]).squeeze(0)
 
         return x_with_pe
 
 class Seq2SeqTransformer(pl.LightningModule):
-    def __init__(self, token_vocab_size, d_model=256, nhead=4, num_layers=2):
+    def __init__(self, token_vocab_size, d_model=256, nhead=4, num_layers=2, dropout=0.1):
         super(Seq2SeqTransformer, self).__init__()
 
         self.validation_step_outputs = []
@@ -34,26 +33,26 @@ class Seq2SeqTransformer(pl.LightningModule):
         self.train_accuracy_outputs = []
         self.validation_accuracy_outputs = []
 
-        self.src_padding_mask = None
-        self.tgt_padding_mask = None
-        self.tgt_causal_mask = None
-
         # Embedding layer
         self.embedding = nn.Embedding(token_vocab_size, d_model)
+        nn.init.orthogonal_(self.embedding.weight)
+
+        # Positional encoding layer
         self.positional_encoding = PositionalEncoding(d_model)
 
-        # Transformer layers
+        # Transformer layers with layer normalization and dropout
         self.transformer = nn.Transformer(
             d_model=d_model,
             nhead=nhead,
             num_encoder_layers=num_layers,
             num_decoder_layers=num_layers,
             batch_first=False,
-            dropout=0.1
+            dropout=dropout
         )
 
-        # Linear layer
+        # Linear layer with layer normalization
         self.fc = nn.Linear(d_model, token_vocab_size)
+        self.layer_norm = nn.LayerNorm(token_vocab_size)
 
     def forward(self, input_ids, target_ids):
         # Embedding
@@ -65,29 +64,137 @@ class Seq2SeqTransformer(pl.LightningModule):
         target_embedding = self.positional_encoding(target_embedding)
 
         # Create padding masks
-        self.src_padding_mask = (input_ids == 0).transpose(0, 1).to(input_ids.device)
-        self.tgt_padding_mask = (target_ids == 0).transpose(0, 1).to(target_ids.device)
+        src_padding_mask = (input_ids == 0).transpose(0, 1).to(input_ids.device)
+        tgt_padding_mask = (target_ids == 0).transpose(0, 1).to(target_ids.device)
 
         # Create the causal mask for the decoder
         tgt_seq_len = target_embedding.size(0)
-        self.tgt_causal_mask = torch.triu(torch.ones(tgt_seq_len, tgt_seq_len), diagonal=1).to(torch.bool).to(target_embedding.device)
+        tgt_causal_mask = torch.triu(torch.ones(tgt_seq_len, tgt_seq_len), diagonal=1).to(torch.bool).to(target_embedding.device)
 
         # Transformer with masks
         output_transformer = self.transformer(input_embedding, target_embedding,
-                                              src_key_padding_mask=self.src_padding_mask,
-                                              tgt_key_padding_mask=self.tgt_padding_mask,
-                                              memory_key_padding_mask=self.src_padding_mask,
-                                              tgt_mask=self.tgt_causal_mask)
+                                              src_key_padding_mask=src_padding_mask,
+                                              tgt_key_padding_mask=tgt_padding_mask,
+                                              memory_key_padding_mask=src_padding_mask,
+                                              tgt_mask=tgt_causal_mask)
 
         # Reshape the output from the transformer to be compatible with the linear layer
         output_flat = output_transformer.view(-1, output_transformer.size(-1))
 
-        output = self.fc(output_flat)
+        # Apply linear layer, layer normalization, and softmax activation
+        output = self.layer_norm(F.softmax(self.fc(output_flat), dim=-1))
 
         # Reshape the output back to (seq_len, batch_size, vocab_size)
         output = output.view(tgt_seq_len, -1, output.size(-1))
 
         return output
+
+
+    def training_step(self, batch, batch_idx):
+        input_ids, target_ids, _ = batch
+
+        target_ids = target_ids.permute(1, 0)
+        input_ids = input_ids.permute(1, 0)
+        
+        # Pass the sequence-first tensors to the transformer
+        output = self(input_ids, target_ids)
+
+        # reshape for loss computation to (seq_len*batch_size, vocab_size)
+        output_reshaped = output.reshape(-1, output.size(-1))
+        target_reshaped = target_ids.reshape(-1)
+
+        # Compute loss
+        loss = F.cross_entropy(output_reshaped, target_reshaped, ignore_index=0)
+
+        # Compute accuracy
+        # Take the argmax of the output to get the most likely token
+        predictions = torch.argmax(output_reshaped, dim=1)
+        # Compute accuracy with masking for padding
+        non_padding_mask = (target_reshaped != 0)
+        correct_count = ((predictions == target_reshaped) & non_padding_mask).sum().item()
+        total_count = non_padding_mask.sum().item()
+        # Avoid division by zero
+        accuracy = correct_count / total_count if total_count > 0 else 0.0
+        accuracy_tensor = torch.tensor(accuracy)
+
+
+        # Log training loss
+        self.log('train_loss', loss, on_epoch=True, prog_bar=True)
+        self.train_step_outputs.append(loss)
+        
+        #Log accuracy
+        self.log('train_accuracy', accuracy, on_epoch=True, prog_bar=True)
+        self.train_accuracy_outputs.append(accuracy_tensor)
+
+        return loss
+
+
+    def validation_step(self, batch, batch_idx):
+        input_ids, target_ids, _ = batch
+
+        target_ids = target_ids.permute(1, 0)
+        input_ids = input_ids.permute(1, 0)
+        
+        # Pass the sequence-first tensors to the transformer
+        # During validation, the model predicts the output without teacher forcing
+        output = self(input_ids, target_ids)
+
+        # Adjust the reshaping to keep the sequence-first format
+        output_reshaped = output.reshape(-1, output.size(-1))
+        target_reshaped = target_ids.reshape(-1)
+
+        # Compute loss
+        loss = F.cross_entropy(output_reshaped, target_reshaped, ignore_index=0)
+
+        # Compute accuracy
+        # Take the argmax of the output to get the most likely token
+        predictions = torch.argmax(output_reshaped, dim=1)
+        # Compute accuracy with masking for padding
+        non_padding_mask = (target_reshaped != 0)
+        correct_count = ((predictions == target_reshaped) & non_padding_mask).sum().item()
+        total_count = non_padding_mask.sum().item()
+        # Avoid division by zero
+        accuracy = correct_count / total_count if total_count > 0 else 0.0
+        accuracy_tensor = torch.tensor(accuracy)
+
+        # Log validation loss
+        self.log('val_loss', loss, on_epoch=True, prog_bar=True)
+        self.validation_step_outputs.append(loss)
+
+        #Log accuracy
+        self.log('val_accuracy', accuracy, on_epoch=True, prog_bar=True)
+        self.validation_accuracy_outputs.append(accuracy_tensor)
+
+        return loss
+
+    def on_validation_epoch_end(self):
+
+        if not len(self.train_step_outputs) == 0:
+            epoch_average_train = torch.stack(self.train_step_outputs).mean()
+            self.log("train_epoch_average", epoch_average_train)
+            print("train_loss_avg: ", epoch_average_train)
+            self.train_step_outputs.clear()
+
+            epoch_train_acc = torch.stack(self.train_accuracy_outputs).mean()
+            self.log("train_accuracy", epoch_train_acc)
+            print("train_acc_avg: ", epoch_train_acc)
+            self.train_accuracy_outputs.clear()
+
+        if not len(self.validation_step_outputs) == 0:
+            epoch_average = torch.stack(self.validation_step_outputs).mean()
+            self.log("validation_epoch_average", epoch_average)
+            print("val_loss_avg: ", epoch_average)
+            self.validation_step_outputs.clear()
+
+            epoch_val_acc = torch.stack(self.validation_accuracy_outputs).mean()
+            self.log("validation_accuracy", epoch_val_acc)
+            print("val_acc_avg: ", epoch_val_acc)
+            self.validation_accuracy_outputs.clear()
+
+
+    def configure_optimizers(self):
+        optimizer = optim.AdamW(self.parameters(), lr=5e-3)
+        return optimizer
 
     def training_step(self, batch, batch_idx):
         input_ids, target_ids, _ = batch
