a0db61d38f147f73ae675ef6dc16ddb39ad0d3e5
Luigi <gallo.1895146@studenti.uniroma1.it>
2024-01-13 22:54:53 +0100

    added plot lightning logs

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,3 +1,7 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+from tensorboard.backend.event_processing import event_accumulator
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader, random_split
 
@@ -28,6 +32,36 @@ def learn_docids(dataset, model, max_epochs, batch_size=1024, **dataloader_kwarg
     trainer = pl.Trainer(max_epochs=max_epochs)
     trainer.fit(model, dataloader)
 
+
+def plot_pl_logs(log_dir, metric, save_dir):
+    # Create save directory if it doesn't exist
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+
+    # Capitalize metric name
+    metric_str = ' '.join(word.capitalize() for word in metric.split('_'))
+
+    # Find the event file in the log directory
+    event_file = next(file for file in os.listdir(log_dir) if 'events.out.tfevents' in file)
+    full_path = os.path.join(log_dir, event_file)
+
+    # Load the TensorBoard event file
+    ea = event_accumulator.EventAccumulator(full_path)
+    ea.Reload()
+
+    # Extracting the scalar 'loss'
+    if metric in ea.scalars.Keys():
+        metric_df = pd.DataFrame(ea.scalars.Items(metric))
+
+        # Plotting
+        plt.figure(figsize=(6, 4))
+        plt.plot(metric_df['step'], metric_df['value'], label=f'{metric_str}')
+        plt.xlabel('Steps')
+        plt.ylabel(f'{metric_str}')
+        plt.title(f'{metric_str} Over Time')
+        plt.savefig(os.path.join(save_dir, f'{metric}.pdf'), bbox_inches='tight')
+    else:
+        print(f"{metric} data not found in logs")
 
 
 
